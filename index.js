fe34ecb085e53019ad7afa7fb4022627802b4d87
okorie2 <44427128+okorie2@users.noreply.github.com>
2020-11-11 01:45:01 +0100

    user page almost done

---
+++ index.js
@@ -0,0 +1,10 @@
+
+/*var i;
+function navElements(){
+    let navItem= document.querySelectorAll(".item-elements");
+    
+    for(i=0; i<navItem.length; i++){
+        navItem[i].style.backgroundColor="#FF6800";
+    }
+    
+}
